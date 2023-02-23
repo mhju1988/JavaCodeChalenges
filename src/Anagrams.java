05603941d3e864621b9d74ed7f538749b4b1c862
Mohamad Juma <Mohamad.Juma@studmail.w-hs.de>
2023-02-23 02:28:20 +0100

    author is added

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -8,7 +8,7 @@ For a given input of two strings, return a Boolean TRUE if the two strings are a
 As an added challenge, for a given array of strings, return separate lists that group anagrams together.
  For example, the input {“tar,” “rat,” “art,” “meats,” “steam”},
  the output should look something like {[“tar,” “rat,” “art”], [“meats,” “steam”]}.*/
-
+//Author Mohamad Juma 23.02.2023
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,8 +16,8 @@ import java.util.List;
 
 public class Anagrams {
 
-    public	ArrayList<ArrayList<String>> finalList = new  ArrayList<>();
-    public 	ArrayList<String> subList = new  ArrayList<>();
+    public ArrayList<ArrayList<String>> finalList = new ArrayList<>();
+    public ArrayList<String> subList = new ArrayList<>();
 
 
     public static boolean areTheWordsAnagrams(String word1, String word2) {
@@ -26,48 +26,50 @@ public class Anagrams {
         String[] charWord2 = word2.split("");
         List<String> wordArray1 = new ArrayList<>();
         List<String> wordArray2 = new ArrayList<>();
-        Arrays.asList(charWord1).forEach(   l -> { wordArray1.add(l.toUpperCase()); }  );
-        Arrays.asList(charWord2).forEach(   k -> { wordArray2.add(k.toUpperCase()); }  );
+        Arrays.asList(charWord1).forEach(l -> {
+            wordArray1.add(l.toUpperCase());
+        });
+        Arrays.asList(charWord2).forEach(k -> {
+            wordArray2.add(k.toUpperCase());
+        });
 
         if (word1.length() != word2.length()) {
             result = false;
         } else if (word1.equalsIgnoreCase(word2) ||
-                wordArray1.stream().allMatch( e -> wordArray2.contains(e)) ) {
+                wordArray1.stream().allMatch(e -> wordArray2.contains(e))) {
             result = true;
         }
         return result;
     }
 
-    public  ArrayList<ArrayList<String>> groupTheSameWords(ArrayList<String> input){
-//        ArrayList<ArrayList<String>> output = new ArrayList<>();
-            for (String word: input ) {
-                if(!subList.contains(word) && !finalList.stream().anyMatch(e ->e.contains(word))) {
-                    subList = new ArrayList<>();
-                    subList.add(word);
-//                ArrayList<String> firstWord = new ArrayList<>();
+    public ArrayList<ArrayList<String>> groupTheSameWords(ArrayList<String> input) {
 
-                    int i = 0;
-                 i =   i < input.size() ? input.indexOf(word) + 1 : input.indexOf(word);
-                    while (i < input.size()) {
-                        if (areTheWordsAnagrams(word, input.get(i))) {
-                            this.subList.add(input.get(i));
-                            i++;
-                        }else{
+        for (String word : input) {
+            if (!subList.contains(word) && !finalList.stream().anyMatch(e -> e.contains(word))) {
+                subList = new ArrayList<>();
+                subList.add(word);
+
+
+                int i = 0;
+                i = i < input.size() ? input.indexOf(word) + 1 : input.indexOf(word);
+                while (i < input.size()) {
+                    if (areTheWordsAnagrams(word, input.get(i))) {
+                        this.subList.add(input.get(i));
+                        i++;
+                    } else {
 
                         i++;
-                        }
-
                     }
-                finalList.add(subList);
-                }
 
+                }
+                finalList.add(subList);
             }
 
-            return finalList;
+        }
+
+        return finalList;
 
     }
-
-
     public static void main(String[] args) {
 
         Anagrams anagrams = new Anagrams();
@@ -91,6 +93,6 @@ public class Anagrams {
         System.out.println(anagrams.groupTheSameWords(hd));
 
 
-
     }
+
 }
