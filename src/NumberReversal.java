bcd0c62096be521a84af4d602a39123cdb793157
Mohamad Juma <Mohamad.Juma@studmail.w-hs.de>
2023-02-24 00:15:49 +0100

    NumberReversal int and double are solved

--- src/NumberReversal.java
+++ src/NumberReversal.java
@@ -11,15 +11,68 @@ The decimal point should stay in the same place.
 // Author Mohamad Juma
 public class NumberReversal {
 
-    public static int reverseNumber(int input){
+    public static int reverseNumber(int input) {
+
+
+        int inputCopy = input;
+        int output = 0;
+        int i = 0;
+        while (input >= 10) {
+            input = input / 10;
+            i++;
+        }
+
+        while (i >= 0) {
+            output = (int) (output + ((inputCopy % 10) * Math.pow((double) 10, (double) i)));
+            inputCopy = inputCopy / 10;
+            i--;
+
+        }
+
+
+        return output;
+    }
+    public static double reverseDoubleNumber(double input){
+
+        double inputCopy = input;
+        double output = 0;
+        int i = 0 ;
+        while (input % 10 > 0.0001 ){
+            input = input * 10 ;
+            i++;
+        }
+        input = (input / 10);
+        i--;
+
+        if(inputCopy > 0 && inputCopy < 1){
+            output = reverseNumber((int) input);
+        }else{
+            output = reverseNumber((int) input) / (Math.pow(10,i));
+        }
 
 
 
-        return 0;
+        return output;
     }
 
     public static void main(String[] args) {
-        System.out.println(1 / 10);
+//        System.out.println(reverseNumber(1234));
+//        System.out.println(22);
+//        System.out.println(reverseNumber(1025));
+//        System.out.println(reverseNumber(3520));
+//        System.out.println(reverseNumber(0));
+//        System.out.println(reverseNumber(2));
+//        System.out.println(reverseNumber(10));
+//        System.out.println(reverseNumber(15));
+//        System.out.println(reverseNumber(20));
+//        System.out.println(reverseNumber(32));
+//        System.out.println(1.543 );  //
+
+
+        System.out.println(reverseDoubleNumber(193.56));
+        System.out.println(reverseDoubleNumber(0.56));
+
+
     }
 
 }
