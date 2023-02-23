c5099d0d954ffba8a08a4b27cd063f3ad1fa453c
mhju1988 <mhju1988@gmail.com>
2023-02-23 21:53:20 +0100

    NumberReversal been created

---
+++ src/NumberReversal.java
@@ -0,0 +1,25 @@
+
+/*This one is a technical interview favorite.
+ For a given input number, return the number in reverse.
+  So, an input of 3956 should return 6593.
+
+If you’re ready for a bigger challenge, reverse a decimal number.
+The decimal point should stay in the same place.
+ So, the number 193.56 should output 653.91.
+ */
+
+// Author Mohamad Juma
+public class NumberReversal {
+
+    public static int reverseNumber(int input){
+
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1 / 10);
+    }
+
+}
